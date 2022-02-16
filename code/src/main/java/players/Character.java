81cd1784b4c6eb5efb782fe21c4f32c62bd82d20
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-16 11:46:16 +0000

    Created Abstract character class

--- code/src/main/java/players/Character.java
+++ code/src/main/java/players/Character.java
@@ -1,4 +1,35 @@
 package players;
 
-public class Character {
+import behaviours.ISwap;
+
+public abstract class Character implements ISwap {
+    private final String name;
+    private int healthPoints;
+    private int gold;
+
+    public Character(String name, int healthPoints) {
+        this.name = name;
+        this.healthPoints = healthPoints;
+        this.gold = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void increaseHP(int i) {
+        healthPoints += i;
+    }
+
+    public void decreaseHP(int i) {
+        healthPoints -= i;
+    }
+
+    public void increaseGold(int i) {
+        gold += i;
+    }
+
+    public void decreaseGold(int i) {
+        gold -= i;
+    }
 }
