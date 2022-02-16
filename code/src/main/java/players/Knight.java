3662662256ba65d129feb3ab055c8acd4c5c14b6
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-16 13:53:42 +0000

    Created Knight class

--- code/src/main/java/players/Knight.java
+++ code/src/main/java/players/Knight.java
@@ -1,4 +1,16 @@
 package players;
 
-public class Knight {
+import powers.Weapon;
+
+public class Knight extends Fighter{
+    int armour;
+
+    public Knight(String name, int healthPoints, Weapon weapon, int armour) {
+        super(name, healthPoints, weapon);
+        this.armour = armour;
+    }
+
+    public int getArmour() {
+        return armour;
+    }
 }
