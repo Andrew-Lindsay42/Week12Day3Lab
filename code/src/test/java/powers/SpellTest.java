efcf07c4af57127501904b1d0b4b2a359635fc36
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-16 14:00:14 +0000

    Added tests for Spell

--- code/src/test/java/powers/SpellTest.java
+++ code/src/test/java/powers/SpellTest.java
@@ -1,4 +1,31 @@
 package powers;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SpellTest {
+
+    Spell spell;
+
+    @Before
+    public void before(){
+        spell = new Spell("Abracadabra Fireball!", 30, 10);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Abracadabra Fireball!", spell.getName());
+    }
+
+    @Test
+    public void hasDamage(){
+        assertEquals(30, spell.getDamage());
+    }
+
+    @Test
+    public void hasManaCost(){
+        assertEquals(10, spell.getManaCost());
+    }
 }
