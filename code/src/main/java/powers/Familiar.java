a74724a26c674373dce9274414fb51acbb2f557c
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-16 14:10:17 +0000

    Created familiar class

--- code/src/main/java/powers/Familiar.java
+++ code/src/main/java/powers/Familiar.java
@@ -1,4 +1,31 @@
 package powers;
 
 public class Familiar {
+    private String name;
+    private String type;
+    private int attack;
+    private int defence;
+
+    public Familiar(String name, String type, int attack, int defence) {
+        this.name = name;
+        this.type = type;
+        this.attack = attack;
+        this.defence = defence;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int attack() {
+        return attack;
+    }
+
+    public int defend() {
+        return defence;
+    }
 }
