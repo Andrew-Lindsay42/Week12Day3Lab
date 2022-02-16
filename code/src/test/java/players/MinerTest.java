bfdf79bf05420d537eedbe4fe8998ca2625f52d3
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-16 11:31:22 +0000

    Empty files and package for powers and behaviours

--- code/src/test/java/players/MinerTest.java
+++ code/src/test/java/players/MinerTest.java
@@ -1,4 +1,13 @@
 package players;
 
+import org.junit.Before;
+
 public class MinerTest {
+
+    Miner miner;
+
+    @Before
+    public void before(){
+        miner = new Miner("Balin", 50);
+    }
 }
