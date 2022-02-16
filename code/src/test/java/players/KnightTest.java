88150f88a5dd125d9ef2a7577554640264f1c7a0
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-16 13:53:31 +0000

    Created test for Knight

--- code/src/test/java/players/KnightTest.java
+++ code/src/test/java/players/KnightTest.java
@@ -1,4 +1,24 @@
 package players;
 
+import org.junit.Before;
+import org.junit.Test;
+import powers.Weapon;
+
+import static org.junit.Assert.assertEquals;
+
 public class KnightTest {
+
+    Knight knight;
+    Weapon weapon;
+
+    @Before
+    public void before(){
+        weapon = new Weapon("Sword", 20);
+        knight = new Knight("Lancelot", 50, weapon, 10);
+    }
+
+    @Test
+    public void hasArmour(){
+        assertEquals(10, knight.getArmour());
+    }
 }
