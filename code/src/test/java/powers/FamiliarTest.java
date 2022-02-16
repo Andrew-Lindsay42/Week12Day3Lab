3a429abf27d4e9b71477b9984944a7c5ce33cae3
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2022-02-16 14:07:51 +0000

    Added tests for familiar

--- code/src/test/java/powers/FamiliarTest.java
+++ code/src/test/java/powers/FamiliarTest.java
@@ -1,4 +1,36 @@
 package powers;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FamiliarTest {
+
+    Familiar familiar;
+
+    @Before
+    public void before(){
+        familiar = new Familiar("Salem", "Cat", 10, 5);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Salem", familiar.getName());
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("Cat", familiar.getType());
+    }
+
+    @Test
+    public void hasAttack(){
+        assertEquals(10, familiar.attack());
+    }
+
+    @Test
+    public void hasDefence(){
+        assertEquals(5, familiar.defend());
+    }
 }
